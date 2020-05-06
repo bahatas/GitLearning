0a0c37b498abcae09dde5278c8b6b6bce3d00c81
bahatas <bahattintastan@yandex.com>
2020-05-06 18:22:38 +0200

    add logCall.java

---
+++ src/tests/logCall.java
@@ -0,0 +1,13 @@
+package tests;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class logCall {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(new String[]{"jo", "ozzy", "Ins"});
+        for (String name : names) {
+            System.out.println("name = " + name);
+        }
+    }
+}
