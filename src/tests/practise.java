dc2815980883cd78429970db667634ea8470c480
bahatas <bahattintastan@yandex.com>
2020-10-30 22:53:07 +0000

    first push to baha branch

--- src/tests/practise.java
+++ src/tests/practise.java
@@ -1,10 +1,16 @@
 package tests;
 
 public class practise {
+    public static void main(String[] args) {
+
+
     // practise -2 again 12 may
     
     // this line written from gitHub. 
     
-    System.out.println("ths line also added");
-    
+    System.out.println("this line also added");
+    // nasıl bşşr sey değişmedi mi
+
+
+    }
 }
